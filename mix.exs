8b110b8381e252b69291e4319d65e4893600b038
QuantumlyTangled <ethoarx@gmail.com>
2020-11-14 09:24:01 +0100

    Initial project commit

---
+++ mix.exs
@@ -0,0 +1,36 @@
+defmodule Gateway.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :gateway,
+      version: "0.1.0",
+      elixir: "~> 1.11",
+      elixirc_paths: elixirc_paths(Mix.env),
+      start_permanent: Mix.env() == :prod,
+      deps: deps()
+    ]
+  end
+
+  # Run "mix help compile.app" to learn about applications.
+  def application do
+    [
+      extra_applications: [:logger]
+    ]
+  end
+
+  # Specifies which paths to compile per environment.
+  defp elixirc_paths(_), do: ["lib"]
+
+  # Run "mix help deps" to learn about dependencies.
+  defp deps do
+    [
+      {:gun, "~> 1.3"},
+      {:amqp, "~> 1.6.0"},
+      {:redix, "~> 1.0"},
+      {:toml, "~> 0.6.2"},
+
+      {:credo, "~> 1.5.1", only: [:dev, :test], runtime: false},
+    ]
+  end
+end

2d3c6d113e6b915ff00cc2da6bb06ea786dd6eb8
QuantumlyTangled <ethoarx@gmail.com>
2020-11-17 22:32:01 +0100

    override recon version

--- mix.exs
+++ mix.exs
@@ -29,9 +29,13 @@ defmodule Gateway.MixProject do
       {:gun, "~> 1.3"},
 
       {:amqp, "~> 1.6.0"},
+      {:manifold, "~> 1.4"},
 
       {:xandra, "~> 0.13.1"},
 
+      {:instruments, "~> 2.1"},
+
+      {:recon, "~> 2.5.1", override: true},
       {:credo, "~> 1.5.1", only: [:dev, :test], runtime: false},
     ]
   end

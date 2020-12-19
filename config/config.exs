562c8c9b8f93515663cfaedfdde9f5956eac0ec0
QuantumlyTangled <ethoarx@gmail.com>
2020-12-19 11:20:39 +0100

    fix an error which I dont fully understand but it works
    For some reason the error was raised in the mix.exs and not the config file

--- config/config.exs
+++ config/config.exs
@@ -6,7 +6,7 @@ config :erikusuaa,
   ],
   gateway: [
     shard_count: System.get_env("GATEWAY_SHARD_COUNT"),
-    identify_delay: String.to_integer(System.get_env("GATEWAY_IDENTIFY_DELAY") || 5100)
+    identify_delay: String.to_integer(System.get_env("GATEWAY_IDENTIFY_DELAY") || "5100")
   ],
   amqp: [
     url: System.get_env("AMQP_URL") || "amqp://localhost:5672"

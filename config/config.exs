98f04c7e8e05f2cfcf1a5788ee63e9fa205e7f74
QuantumlyTangled <ethoarx@gmail.com>
2020-12-19 11:18:28 +0100

    use env for config values

--- config/config.exs
+++ config/config.exs
@@ -3,4 +3,11 @@ use Mix.Config
 config :erikusuaa,
   bot: [
     token: System.get_env("BOT_TOKEN")
+  ],
+  gateway: [
+    shard_count: System.get_env("GATEWAY_SHARD_COUNT"),
+    identify_delay: String.to_integer(System.get_env("GATEWAY_IDENTIFY_DELAY") || 5100)
+  ],
+  amqp: [
+    url: System.get_env("AMQP_URL") || "amqp://localhost:5672"
   ]

7ed04bfb787fb9213ae70acdc6970b69d85773c9
Atomiqu <69437803+Atomiqu@users.noreply.github.com>
2021-10-31 08:01:33 +0100

    Update mix.exs

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Erikusuaa.MixProject do
     [
       app: :erikusuaa,
       version: "0.1.0",
-      elixir: "~> 1.10",
+      elixir: "~> 1.12",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       deps: deps(),

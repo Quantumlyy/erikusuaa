467a0633044ed40787787b8e1c1fffacb46783b1
Atomiqu <69437803+Atomiqu@users.noreply.github.com>
2021-10-31 08:25:29 +0100

    [chore] upgrade elixir (#1)

--- lib/erikusuaa.ex
+++ lib/erikusuaa.ex
@@ -2,17 +2,4 @@ defmodule Erikusuaa do
   @moduledoc """
   Documentation for `Erikusuaa`.
   """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Erikusuaa.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
